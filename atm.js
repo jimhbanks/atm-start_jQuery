f92a4e8a95ef1a63da12fb07cf6400519a362c90
Jim Banks <iamjimhbanks@me.com>
2015-05-06 19:18:43 +0100

    capturing numbers but returning balance as NaN

--- atm.js
+++ atm.js
@@ -1,23 +1,22 @@
 $(document).ready(function(){
  var balance = ''
 
-// deposits #amount1 to #balance1 but isnt storing
+// deposits #amount1 to #balance1, does not store
   $('#deposit1').click(function(){
   $('#balance1').html($('#amount1').val());
    balance = $('#amount1');
-  debugger;
    })
 
-});
- 
-//  $('#withdrawl1').click(function(){
-//     balance = $('#amount1').cal();
-//   $('#balance1').html($('#amount1').val());
-//    return this.balance = $('#amount1');
-// });
-//    })
+//withdraws inputted balance and updates new balance
+  $('#withdraw1').click(function(){
+   withdraw = $('#amount1').val();
+   balance = balance - withdraw;
+   $('#balance1').html(balance);
+  })
+   })
 
-// });
+
+ 
 
 
 
