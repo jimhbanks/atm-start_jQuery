ffe60979f0e069c0377787d98048e44c2a868283
Jim Banks <iamjimhbanks@me.com>
2015-05-06 19:07:57 +0100

    this time it ACTUALLY is returning a value

--- atm.js
+++ atm.js
@@ -3,14 +3,21 @@ $(document).ready(function(){
 
 // deposits #amount1 to #balance1 but isnt storing
   $('#deposit1').click(function(){
-    balance = $('#amount1').cal();
   $('#balance1').html($('#amount1').val());
-   return this.balance = $('#amount1');
- 
+   balance = $('#amount1');
+  debugger;
    })
 
 });
+ 
+//  $('#withdrawl1').click(function(){
+//     balance = $('#amount1').cal();
+//   $('#balance1').html($('#amount1').val());
+//    return this.balance = $('#amount1');
+// });
+//    })
 
+// });
 
 
 
