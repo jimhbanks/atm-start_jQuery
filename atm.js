b623686ac14fa7d81fbe19ba857ed3cafb3155ac
Jim Banks <iamjimhbanks@me.com>
2015-05-06 18:13:45 +0100

    gets input from dep1 (input box) and shows in display (amount1)

--- atm.js
+++ atm.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-  
-})
+
+    var balance = ''
+
+// gets input from dep1 (input box) and shows in display (amount1)
+       $('#deposit1').click(function(){
+        $('#balance').html($('#amount1').val());
+        balance = $this.val
+      var value = $('balance').val();
+
+
+       $('#withdraw1').click(function(){
+        $('#balance').html($('#amount1').val());
+
+});
+
+        }); 
+
+
 
 
 
