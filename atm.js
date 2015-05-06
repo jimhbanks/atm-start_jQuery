53e8c758862aa0cc811c82e22ae206089b7e5c61
Jim Banks <iamjimhbanks@me.com>
2015-05-06 19:01:49 +0100

    returning value

--- atm.js
+++ atm.js
@@ -1,21 +1,15 @@
 $(document).ready(function(){
+ var balance = ''
 
-    var balance = ''
-
-// gets input from dep1 (input box) and shows in display (amount1)
-       $('#deposit1').click(function(){
-        $('#balance').html($('#amount1').val());
-        balance = $this.val
-      var value = $('balance').val();
-
-
-       $('#withdraw1').click(function(){
-        $('#balance').html($('#amount1').val());
+// deposits #amount1 to #balance1 but isnt storing
+  $('#deposit1').click(function(){
+    balance = $('#amount1').cal();
+  $('#balance1').html($('#amount1').val());
+   return this.balance = $('#amount1');
+ 
+   })
 
 });
-
-        }); 
-
 
 
 
